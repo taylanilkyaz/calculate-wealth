a01fc4bf1d85ca479125daffbee9aa600ff11e3b
taylanilkyaz <taylan.ilkyaz@kboxglobal.app>
2020-11-16 00:32:41 +0300

    add sample http requests

--- backend/app.js
+++ backend/app.js
@@ -2,15 +2,15 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
+const cors = require('cors');
 require('dotenv/config');
 
+app.use(cors());
 app.use(bodyParser.json());
 
 //Import Routes
 const postsRoute = require('./routes/posts');
 app.use('/posts', postsRoute);
-
 
 //ROUTES
 app.get('/', (req, res) => {
--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -2,15 +2,17 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post')
 
+//Get Users
 router.get('/', async (req, res) => {
     try {
         const posts = await Post.find();
         res.json(posts);
-    } catch (error) {
-        res.json({ message:  err});
+    } catch (err) {
+        res.json({ message: err });
     }
 });
 
+//Submit User
 router.post('/', async (req, res) => {
     const post = new Post({
         title: req.body.title,
@@ -24,9 +26,39 @@ router.post('/', async (req, res) => {
     }
 })
 
-router.get('/spesific', (req, res) => {
-    res.send('We are at spesific posts');
+//Get Spesific User
+router.get('/:postId', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.postId);
+        res.json(post);
+    } catch (err) {
+        res.json({ message: err });
+    }
 });
 
+//Delete User
+router.delete('/:postId', async (req, res) => {
+    try {
+        const removedPost = await Post.remove({ _id: req.params.postId });
+        res.json({ message: 'Delete process is successfull' })
+    } catch (err) {
+        res.json({ message: err });
+    }
+});
+
+//Update User
+router.put('/:postId', async (req, res) => {
+    try {
+        const updatedPost = await Post.updateOne(
+            { _id: req.params.postId },
+            {
+                $set: { title: req.body.title, description: req.body.description }
+            });
+        res.json(updatedPost.body)
+    } catch (err) {
+        res.json({ message: err });
+    }
+
+});
 
 module.exports = router;
