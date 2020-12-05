63bee6578dffc3192fdad64d99ea3d9646a65c8c
taylanilkyaz <taylan.ilkyaz@kboxglobal.app>
2020-12-05 20:26:05 +0300

    add bootstrap and fontawesome libraries

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Navbar from "./components/Navbar";
 import './App.css';
+import User from "./components/User"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <Navbar title="User App 2" />
+        <hr />
+        <User
+          name="Taylan İlkyaz"
+          
+          salary="5000" />
+        <User
+          name="Ayşe Akcan İlkyaz"
+          department="software"
+          salary="5000" />
+      </div>
+    )
+  }
 }
 
 export default App;
